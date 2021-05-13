e08be32d13615afe6b6efe6c5398706e702cd73d
Sushant <sushant.adhikari.619@gmail.com>
2021-05-13 12:00:30 +0545

    authenticated edit, delete, image upload

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -2,6 +2,7 @@ import moment from 'moment'
 import {FaImage} from 'react-icons/fa'
 import {ToastContainer, toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import {parseCookies} from '@/helpers/index'
 import {useState} from 'react'
 import {useRouter} from 'next/router'
 import Link from 'next/link'
@@ -12,7 +13,7 @@ import Modal from '@/components/Modal'
 import ImageUpload from '@/components/ImageUpload'
 import styles from '@/styles/Form.module.css'
 
-export default function EditEventPage({evt}) {
+export default function EditEventPage({evt, token}) {
     const [values, setValues] = useState({
         name: evt.name,
         performers: evt.performers,
@@ -41,12 +42,17 @@ export default function EditEventPage({evt}) {
         const res = await fetch(`${API_URL}/events/${evt.id}`, {
             method: 'PUT',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
             },
             body: JSON.stringify(values)
         })
 
         if(!res.ok) {
+            if(res.status === 403 || res.status === 401) {
+                toast.error('Unauthorized')
+                return
+            }
             toast.error('Something Went Wrong')
         } else {
             const evt = await res.json()
@@ -159,21 +165,22 @@ export default function EditEventPage({evt}) {
                 </div>
 
         <Modal show={showModal} onClose={() => setShowModal(false)}>
-            <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+            <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} token={token} />
         </Modal>
         </Layout>
     )
 }
 
 export async function getServerSideProps({params: {id}, req}) {
+    const {token} = parseCookies(req)
+
     const res = await fetch(`${API_URL}/events/${id}`)
     const evt = await res.json()
-
-    console.log(req.headers.cookie)
 
     return {
         props: {
             evt,
+            token
         },
     }
 }
